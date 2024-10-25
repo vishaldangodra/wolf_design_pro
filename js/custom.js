867564ac65c1cb86cf2f12f58ddf4573ec91782d
unknown <vishaldangodra2@gmail.com>
2024-10-25 11:41:55 +0530

    degine code

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: true,
+    items: 3,
+    autoplay: true,
+    autoplayTimeout: 1000,
+    autoplayHoverPause: true
+    // responsive:{
+    //     0:{
+    //         items:1
+    //     },
+    //     600:{
+    //         items:3
+    //     },
+    //     1000:{
+    //         items:5
+    //     }
+    // }
+})
